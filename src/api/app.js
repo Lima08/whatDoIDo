229f9d4f92245afed681d8a26c8318ee938eab87
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-10-31 20:06:54 -0300

    CORS FEito a configuraçao de cors.

--- src/api/app.js
+++ src/api/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const userRouter = require('../routers/userRoutes');
 const todoRouter = require('../routers/todoRoutes');
 const middlewareError = require('../middlewares/error');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
